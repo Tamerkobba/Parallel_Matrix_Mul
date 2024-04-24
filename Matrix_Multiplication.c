b92274e29fde315bd7779fbdd13bd1fb85c947ed
Tamerkobba <126768205+Tamerkobba@users.noreply.github.com>
2024-04-24 10:15:05 +0300

    Update Matrix_Multiplication.c

--- Matrix_Multiplication.c
+++ Matrix_Multiplication.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-void matrixMul(int *a, int *b, int *c, int N) {
+void matrixMul(int *a, int *b, int *c, int N, int K, int M) {
   for (int row = 0; row < N; row++) {
-    for (int col = 0; col < N; col++) {
-      c[row * N + col] = 0;
-      for (int k = 0; k < N; k++) {
-        c[row * N + col] += a[row * N + k] * b[k * N + col];
+    for (int col = 0; col < M; col++) {
+      c[row * M + col] = 0;
+      for (int k = 0; k < K; k++) {
+        c[row * M + col] += a[row * K + k] * b[k * M + col];
       }
     }
   }
@@ -20,29 +20,36 @@ double getTime() {
 }
 
 int main() {
-  int N = 1 << 10;
-  size_t size = N * N * sizeof(int);
+  int N = 10; // Number of rows in A and C
+  int K = 20;  // Number of columns in A and rows in B
+  int M = 10;  // Number of columns in B and C
+
+  size_t sizeA = N * K * sizeof(int);
+  size_t sizeB = K * M * sizeof(int);
+  size_t sizeC = N * M * sizeof(int);
 
   // host copies of matrices a, b & c
-  int *h_a = (int *)malloc(size);
-  int *h_b = (int *)malloc(size);
-  int *h_c = (int *)malloc(size);
+  int *h_a = (int *)malloc(sizeA);
+  int *h_b = (int *)malloc(sizeB);
+  int *h_c = (int *)malloc(sizeC);
 
   // Setup input values
-  for (int i = 0; i < N * N; i++) {
+  for (int i = 0; i < N * K; i++) {
     h_a[i] = rand() % 100;
+  }
+  for (int i = 0; i < K * M; i++) {
     h_b[i] = rand() % 100;
   }
 
   double start = getTime();
-  matrixMul(h_a, h_b, h_c, N);
+  matrixMul(h_a, h_b, h_c, N, K, M);
   double end = getTime();
 
   printf("Result Matrix:\n");
   // Print a subset of the result matrix
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
-      printf("%d ", h_c[i * N + j]);
+      printf("%d ", h_c[i * M + j]);
     }
     printf("\n");
   }
